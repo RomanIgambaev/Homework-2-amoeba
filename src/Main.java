41959b7dde9044a8d909d7c7d0a5286cf7b5fe59
RomanIgambaev <roman.igambaev11@gmail.com>
2023-08-16 15:09:20 +0500

    Amoeba

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int amoeba = 1;
+        int coint = 1;
+        while (coint <= 8) {
+            amoeba = amoeba*2;
+            coint++;
+            System.out.println("Кол во амёб каждые три часа: " + amoeba);
+        }
     }
 }
